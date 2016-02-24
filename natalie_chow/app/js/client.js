82a8af060a7cebaf7e685c506d00e54f734096b4
Natalie Chow <cychownat@gmail.com>
2016-02-24 10:08:16 -0800

    Added additional content to fulfill 3-column layout

--- natalie_chow/app/js/client.js
+++ natalie_chow/app/js/client.js
@@ -39,6 +39,23 @@ salemApp.controller('UsersController', ['$scope', '$http', function($scope, $htt
 }]);
 
 
+salemApp.controller('NonCrudController', ['$scope', '$http', function($scope, $http) {
+  $scope.population = {
+    towns: 0,
+    mafias: 0,
+    totalPopulation: 0
+  };
+
+  $scope.census = function() {
+    $http.get('http://localhost:3000/census')
+      .then((res) => {
+        $scope.population = res.data;
+      }, (err) => {
+        console.log(err);
+      });
+  };
+}]);
+
 salemApp.controller('TownsController', ['$scope', '$http', function($scope, $http) {
   $scope.towns = [];
 
